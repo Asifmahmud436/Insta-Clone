e14b59de99c6264729e206ab3d1ba5a326683b84
Zettex <safaandsafa4@gmail.com>
2024-10-07 12:16:21 +0600

    post model updated for image

--- posts/models.py
+++ posts/models.py
@@ -5,6 +5,7 @@ class Post(models.Model):
     author = models.ForeignKey(User,on_delete=models.CASCADE)
     title = models.CharField(max_length=60)
     body = models.TextField()
+    image = models.ImageField(upload_to='posts/images/')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
